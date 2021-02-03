d287beabda6cabfe727e126a4c6439e7b977c463
dougnoel <dougnoel@gmail.com>
2021-02-03 17:02:03 -0500

    Update to work with sentinel 1.0.5

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -1,43 +1,45 @@
 package tests;
 
-import java.io.File;
+import java.io.IOException;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-import com.cucumber.listener.Reporter;
+import com.dougnoel.sentinel.exceptions.SentinelException;
 import com.dougnoel.sentinel.pages.PageManager;
 import com.dougnoel.sentinel.webdrivers.WebDriverFactory;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(monochrome = true
-        , features = "src/test/java/features"
-        , glue = { "stepdefinitions", "com.dougnoel.sentinel.steps" }
-        , plugin = { "com.cucumber.listener.ExtentCucumberFormatter:reports/extent-cucumber-report.html" }
+	, features = "src/test/java/features"
+	, glue = { "stepdefinitions", "com.dougnoel.sentinel.steps" }
+	, plugin = {"json:target/cucumber.json",
+			"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+	, strict = true
 //      , tags = { "@19" }
 )
 
 public class TestRunner {
-
+    private static final Logger log = LogManager.getLogger(TestRunner.class); // Create a logger.
+    
     @BeforeClass
-    public static void setUpBeforeClass() throws Exception {
-        System.setProperty("env", "demo"); // Set the environment (dev/qa/stage/prod/etc)
+    public static void setUpBeforeClass() throws IOException, SentinelException {
         System.setProperty("org.freemarker.loggerLibrary", "none");
+        System.setProperty("env", "demo"); // Set the environment (dev/qa/stage/prod/etc)
         WebDriverFactory.instantiateWebDriver();
     }
 
     @AfterClass
-    public static void tearDownAfterClass() throws Exception {
-    	PageManager.quit();
-        
-        Reporter.loadXMLConfig(new File("conf/extent-config.xml"));
-        Reporter.setSystemInfo("user", System.getProperty("user.name"));
-        Reporter.setSystemInfo("os", System.getProperty("os"));
-        Reporter.setTestRunnerOutput("Sentinel test runner output");
+    public static void tearDownAfterClass() throws SentinelException {
+        log.info("Driver: {}", WebDriverFactory.getWebDriver());
+        if (System.getProperty("leaveBrowserOpen", "false") == "false") {
+        	PageManager.quit();
+        }
     }
-
 }
