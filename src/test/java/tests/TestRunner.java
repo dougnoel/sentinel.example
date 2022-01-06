162834be8b31b1de8d345f684ccabb0f906ed158
Doug Noel <dnoel@metabolon.com>
2022-01-06 12:32:06 -0500

    Fixed test runner.

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -3,12 +3,10 @@ package tests;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import com.dougnoel.sentinel.configurations.Configuration;
-import com.dougnoel.sentinel.webdrivers.WebDriverFactory;
-
+import com.dougnoel.sentinel.webdrivers.Driver;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
@@ -23,11 +21,6 @@ import io.cucumber.junit.CucumberOptions;
 
 public class TestRunner {
     private static final Logger log = LogManager.getLogger(TestRunner.class); // Create a logger.
-
-	@BeforeClass
-	public static void setUpBeforeAnyTestsAreRun() {
-		WebDriverFactory.instantiateWebDriver();
-	}
 	
     @AfterClass
     public static void tearDownAfterClass() {
@@ -35,9 +28,9 @@ public class TestRunner {
         if (totalWaitTime != null) {
         	log.warn("This test took {} total seconds longer due to explicit waits. Sentinel handles dynamic waits. If you have a reason for adding explicit waits, you should probably be logging a bug ticket to get the framework fixed at: http://https://github.com/dougnoel/sentinel/issues", totalWaitTime);
         }
-    	log.info("Driver: {}", WebDriverFactory.getWebDriver());
+        
         if (System.getProperty("leaveBrowserOpen", "false") == "false") {
-        	WebDriverFactory.quit();
+        	Driver.quit();
         }
     }
 }
